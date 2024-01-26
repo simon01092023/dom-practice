4aa49dddbaac844147d6a64db9e0ef7a26bf0712
simon01092024 <simonkudus510@gmail.com>
2024-01-26 11:35:28 -0800

    Just saving progess so far

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const game = {
+    score: 10,
+    players: ['Jim', 'Liz']
+};
+
+const ulEl = document.querySelector('ul');
+
+game.players.forEach(function(player){
+    const liEl = document.createElement('li');
+    liEl.innerText = player
+    console.log(liEl)
+    ulEl.appendChild(liEl)
+});
+
+
+
+const scoreEl = document.querySelector('#score')
+console.log(scoreEl)
+scoreEl.innerText = printScore(100);
+
+const newE = document.querySelector('#newElement')
+newE.innerText = "New Element!";
+
+
+function printScore(){
+    return("the game score is " + game.score);
+} 
+
+
+game.players.forEach(function printPlayer(player){
+    console.log(`${player}`)
+});
